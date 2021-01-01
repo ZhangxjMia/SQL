1fa590ad33004bb822e0f3631a50481a132400c5
Mia (Xiaojia) Zhang <63559049+ZhangxjMia@users.noreply.github.com>
2020-12-31 16:01:08 -0800

    PostgreSQL Exercises

--- modifying_data.sql
+++ modifying_data.sql
@@ -90,7 +90,9 @@ Delete all bookings.
 As part of a clearout of our database, we want to delete all bookings from the cd.bookings table. How can we accomplish this?
 */
 DELETE FROM cd.bookings;
-
+/*
+bookid	 facid	 memid	 starttime	slots
+*/
 
 
 /*
