c8bb10cd70e5c9aa83601c746b75ba35129f659a
Mia (Xiaojia) Zhang <63559049+ZhangxjMia@users.noreply.github.com>
2021-01-02 15:45:41 -0800

    PostgreSQL Exercises

--- modifying_data.sql
+++ modifying_data.sql
@@ -71,16 +71,16 @@ SET membercost = (SELECT membercost*1.1 FROM cd.facilities WHERE facid = 0),
     guestcost = (SELECT guestcost*1.1 FROM cd.facilities WHERE facid = 1)
 WHERE fac.facid = 1;
 /*
-facid	name		membercost	guestcost	initialoutlay	monthlymaintenance
+facid	name		       membercost	guestcost	initialoutlay	monthlymaintenance
 0	Tennis Court 1	5		25		10000		200
 1	Tennis Court 2	5.5		27.5		8000		200
 2	Badminton Court	0		15.5		4000		50
-3	Table Tennis	0		5		320		10
+3	Table Tennis	       0	       5		320		10
 4	Massage Room 1	35		80		4000		3000
 5	Massage Room 2	35		80		4000		3000
-6	Squash Court	3.5		17.5		5000		80
-7	Snooker Table	0		5		450		15
-8	Pool Table	0		5		400		15
+6	Squash Court	       3.5		17.5		5000		80
+7	Snooker Table	       0		5		450		15
+8	Pool Table	       0		5		400		15
 */
 
 
